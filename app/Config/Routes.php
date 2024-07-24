8c942d508a2b15234c14299d2d3169f687c81f1a
subhomoy samanta <subhomoy@keylines.net>
2024-07-24 13:29:45 +0530

    attendance note modifiy api implemented

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -201,6 +201,7 @@ $routes->post('/client-Details-Data', 'Home::clientDetailsData/');
 			$routes->match(['post'], "get-single-attendance", "ApiController::getSingleAttendance");
 			
 			$routes->match(['post'], "get-notifications", "ApiController::getNotifications");
+			$routes->match(['post'], "update-note", "ApiController::updateNote");
 		// after login
 	});
 /* API */
