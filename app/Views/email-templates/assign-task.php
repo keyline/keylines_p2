5bbd7cc26679c4ddfb90e877d3f4997aa30f782b
subhomoy samanta <subhomoy@keylines.net>
2024-08-05 12:23:21 +0530

    user module bug fixing

--- app/Views/email-templates/assign-task.php
+++ app/Views/email-templates/assign-task.php
@@ -13,7 +13,7 @@ $generalSetting     = $this->common_model->find_data('general_settings', 'row');
   <div style="padding: 20px;">
     <div style="text-align: center;"><img style="max-width: 250px;" src="<?=getenv('app.uploadsURL').$generalSetting->site_logo?>" alt="<?=$generalSetting->site_name?>"></div>    
     <div style="background: #f5e6e6; padding: 15px;">
-      <h3 style="color: #E40E11; font-family: Arial; margin-top: 0">New Tsk is assigned in KEYLINE TRACKER</h3>
+      <h3 style="color: #E40E11; font-family: Arial; margin-top: 0">New task is assigned in KEYLINE TRACKER</h3>
       <p><b><?=$assign_user?>,</b></p>
       <p>Here is your Task Details -</p>
       <p><b>Project Name: </b><a href="<?=$project_url?>" target="_blank"><?=$project_name?></a>(<?=$project_status?>)</p>
