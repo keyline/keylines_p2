370b0c01f50b2cdce7faba7a31032402b3bd47d8
subhomoy samanta <subhomoy@keylines.net>
2024-08-16 13:29:50 +0530

    tracker different module bug fixing

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -179,9 +179,9 @@ abstract class BaseController extends Controller
             else:
                 $mailLibrary->addAddress($email);
             endif;
-            $mailLibrary->addCC('sudip.keyline@gmail.com', 'KDPL System');
+            // $mailLibrary->addCC('sudip.keyline@gmail.com', 'KDPL System');
             $mailLibrary->addCC('subhomoy@keylines.net', 'Subhomoy Samanta');
-            $mailLibrary->addCC('deblina@keylines.net', 'Deblina Das');
+            // $mailLibrary->addCC('deblina@keylines.net', 'Deblina Das');
             $mailLibrary->WordWrap      = 5000;
             $mailLibrary->Subject       = $subject;
             $mailLibrary->Body          = $message;
