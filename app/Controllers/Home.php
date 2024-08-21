6b2e8fb0432f93d7fb2eac0595e6a9fa750936e1
Deblina Das <deblina@keylines.net>
2024-08-21 13:46:41 +0530

    cron issue fixing

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -283,7 +283,7 @@ class Home extends BaseController
                         $month  =   date('m');
                          $sql10 = "SELECT * FROM `desktime_sheet_tracking` WHERE year_upload = '$year' AND month_upload = '$month' AND user_id = '$user_id'";
                         $getDesktimeHour = $this->db->query($sql10)->getRow();                        
-                        // echo $sql = "SELECT time_at_work FROM `desklog_report` where tracker_user_id='$user_id' and insert_date LIKE '%" . date('Y').'-'.date('m') . "%'";
+                        $sql = "SELECT time_at_work FROM `desklog_report` where tracker_user_id='$user_id' and insert_date LIKE '%" . date('Y').'-'.date('m') . "%'";
                         $getDesktime = $this->db->query($sql)->getResult();                        
                         $totalHours = 0;
                         $totalMinutes = 0;
