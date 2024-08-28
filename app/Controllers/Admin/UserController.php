f6380b09032ae2eff8596f3bb0640f1979ce8fe8
Deblina Das <deblina@keylines.net>
2024-08-28 17:51:40 +0530

    user cost module modification

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -317,6 +317,7 @@ class UserController extends BaseController {
         $order_by[0]                = array('field' => $this->data['primary_key'], 'type' => 'desc');
         $userType                   = $this->session->user_type;        
         $data['rows']               = $this->data['model']->find_data($this->data['table_name'], 'array', ['status' => '1'], '', '', '', $order_by);                
+        echo $this->db->getLastquery(); die;
         if($this->request->getMethod() == 'post') {
             $id = $this->request->getPost('id');            
             $postData   = array(
