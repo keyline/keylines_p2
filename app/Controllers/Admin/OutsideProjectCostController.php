459e72b080492a88ab8f8e9179109a819e163140
Deblina Das <deblina@keylines.net>
2024-09-06 13:09:17 +0530

    all changes according to sir feedback

--- app/Controllers/Admin/OutsideProjectCostController.php
+++ app/Controllers/Admin/OutsideProjectCostController.php
@@ -2,6 +2,7 @@
 namespace App\Controllers\admin;
 use App\Controllers\BaseController;
 use App\Models\CommonModel;
+use App\Libraries\Pro;
 class OutsideProjectCostController extends BaseController {
 
     private $model;  //This can be accessed by all class methods
