9281b3aeae1cccc86ae599ef13fcaf0a882d62ee
subhomoy samanta <subhomoy@keylines.net>
2024-10-07 20:03:53 +0530

    task assign module update

--- app/Views/admin/maincontents/effort/previous-schedule-html.php
+++ app/Views/admin/maincontents/effort/previous-schedule-html.php
@@ -174,7 +174,7 @@ $generalSetting             = $common_model->find_data('general_settings', 'row'
                                                         <div class="d-flex justify-content-between">
                                                             <p class="mb-0 assign-name">
                                                                 By <?=$getTask->user_name?> <span class="ms-1">(<?=$createdAt?>)</span>
-                                                                <?=$getTask->work_status_id?>/<?=$effortIcon?>/<?=$user_id?>/<?=$type?>
+                                                                <?=$getTask->work_status_id?>/<?=$effortIcon?>/<?=$user_id?>/<?=$type?>/<?=$teamMember->id?>
                                                                 <?php if($getTask->work_status_id == 0){?>
                                                                     <?php if($effortIcon){?>
                                                                         <br>
