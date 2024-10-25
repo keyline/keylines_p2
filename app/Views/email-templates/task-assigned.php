3cc9a318783d75cd8fb568d3f264869fd665b715
subhomoy samanta <subhomoy@keylines.net>
2024-10-25 13:27:41 +0530

    task assign module updated

--- app/Views/email-templates/task-assigned.php
+++ app/Views/email-templates/task-assigned.php
@@ -13,12 +13,24 @@ $generalSetting     = $this->common_model->find_data('general_settings', 'row');
   <div style="padding: 20px;">
     <div style="text-align: center;"><img style="max-width: 250px;" src="<?=getenv('app.uploadsURL').$generalSetting->site_logo?>" alt="<?=$generalSetting->site_name?>"></div>    
     <div style="background: #f5e6e6; padding: 15px;">
-      <h3 style="color: #E40E11; font-family: Arial; margin-top: 0">New task is assigned in KEYLINE TRACKER</h3>
-      <p><b><?=$assign_user?>,</b></p>
+      <h3 style="color: #E40E11; font-family: Arial; margin-top: 0"><?=$subject?></h3>
+      <p><b><?=$added_by?>,</b></p>
       <p>Here is your Task Details -</p>
-      <p><b>Project Name: </b><a href="<?=$project_url?>" target="_blank"><?=$project_name?></a>(<?=$project_status?>)</p>
-      <p><b>Assigned By: </b><?=$assigned_by?></p>
+      <p><b>Project Name: </b><?=$project_name?></p>
+      <p><b>Assigned Time: </b><?=$date_added?> - <?=$hour?>:<?=$min?></p>
+      <p><b>Assigned By: </b><?=$added_by?></p>
       <p><b>Task description: </b><?=$description?></p>
+      <p><b>Priority: </b>
+        <?php if($priority == 3){?>
+            <span class="card_priotty_item proiodty_high">HIGH</span>
+        <?php }?>
+        <?php if($priority == 2){?>
+            <span class="card_priotty_item proiodty_medium">MEDIUM</span>
+        <?php }?>
+        <?php if($priority == 1){?>
+            <span class="card_priotty_item proiodty_low">LOW</span>
+        <?php }?>
+      </p>
     </div>    
   </div>
 </body>
