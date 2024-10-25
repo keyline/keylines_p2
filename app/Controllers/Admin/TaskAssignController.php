714170bb4e7b9bb47a5b690d7567a49cad0a4657
subhomoy samanta <subhomoy@keylines.net>
2024-10-25 14:09:44 +0530

    task assign module updated

--- app/Controllers/Admin/TaskAssignController.php
+++ app/Controllers/Admin/TaskAssignController.php
@@ -1459,8 +1459,9 @@ class TaskAssignController extends BaseController {
                 $generalSetting             = $this->common_model->find_data('general_settings', 'row');
                 $getProject                 = $this->common_model->find_data('project', 'row', ['id' => $requestData['project_id']], 'name');
                 $getAssignedTask            = $this->common_model->find_data('timesheet', 'row', ['assigned_task_id' => $schedule_id]);
-                $added_by                   = (($getAssignedTask)?$getAssignedTask->user_id:'');
+                echo $added_by                   = (($getAssignedTask)?$getAssignedTask->user_id:'');
                 $getUser                    = $this->common_model->find_data('user', 'row', ['id' => $added_by], 'name,email');
+                pr($getUser);
                 $subject                    = $generalSetting->site_name.' :: Effort Booked'.(($getAssignedTask)?date_format(date_create($getAssignedTask->date_today), "M d, Y"):'').' '.(($getProject)?$getProject->name:'').' - '.$requestData['hour'].':'.$requestData['min'];
                 $mailData                   = [
                     'subject'                   => $subject,
