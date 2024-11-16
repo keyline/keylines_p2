972f7e5b4ca70e691182ac7246f2705bf8a1aaf7
debojyti <debojyti@keylines.net>
2024-11-16 15:52:22 +0530

    fetch desklog report cron check

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -190,7 +190,8 @@ class Home extends BaseController
                 foreach ($data as $item) {
                     $db_date = date_format(date_create($cu_date), "Y-m-d");
                     $existingRecord = $this->common_model->find_data('desklog_report', 'row', ['desklog_usrid' => $item['id'], 'insert_date LIKE' => '%'.$db_date.'%']);
-                        pr($existingRecord);
+                    echo $this->db->getLastquery();die;
+                        // pr($existingRecord);
                     if(!$existingRecord){
                         // echo "user insert"; die;
                         $postData   = array(
