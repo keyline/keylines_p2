c5c4a3250da046341f149bd0c333814a8fe08497
debojyti <debojyti@keylines.net>
2024-11-16 15:58:05 +0530

    fetch desklog report cron check

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -181,7 +181,7 @@ class Home extends BaseController
                 $url = $apiUrl . '?appKey=' . $appKey . '&date=' . $cu_date;
                 $response = file_get_contents($url);
                 $data = json_decode($response, true);  
-                // pr($data);
+                 pr($data);
             } catch (Exception $e) {
                 // Log the error message
                 log_message('error', 'API call failed: ' . $e->getMessage());
@@ -190,7 +190,7 @@ class Home extends BaseController
             foreach ($data as $item) {
                 $db_date = date_format(date_create($cu_date), "Y-m-d");
                 $existingRecord = $this->common_model->find_data('desklog_report', 'row', ['desklog_usrid' => $item['id'], 'insert_date LIKE' => '%' . $db_date . '%']);
-                echo $this->db->getLastquery();die;
+                // echo $this->db->getLastquery();die;
                 if (!$existingRecord) {
                     $postData   = array(
                         'desklog_usrid' => $item['id'],
