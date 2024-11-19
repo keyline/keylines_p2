0c51e15aaaef5337b7ff20c067bb8d5b2e158481
subhomoy samanta <subhomoy@keylines.net>
2024-11-19 14:38:40 +0530

    project hour cost report updated

--- app/Controllers/Admin/ReportController.php
+++ app/Controllers/Admin/ReportController.php
@@ -1234,17 +1234,21 @@ class ReportController extends BaseController
                                     <tbody>';
         if ($ongoingProjects) { $sl = 1;$total_cost = 0; foreach ($ongoingProjects as $ongoingProject) {
                 /* cost calculation */
-                    $cost_sql1      = "SELECT project_cost FROM `project_cost` WHERE month=$last_month_month AND year=$last_month_year AND project_id = $ongoingProject->project_id";
-                    $checkCost      = $this->db->query($cost_sql1)->getRow();
+                    // $cost_sql1      = "SELECT project_cost FROM `project_cost` WHERE month=$last_month_month AND year=$last_month_year AND project_id = $ongoingProject->project_id";
+                    // $checkCost      = $this->db->query($cost_sql1)->getRow();
                     $project_cost   = 0;
-                    if($checkCost){
-                        $project_cost   = $checkCost->project_cost;
-                    } else {
-                        $date_added     = $last_month_year.'-'.$last_month_month;
-                        $cost_sql2      = "SELECT sum(cost) as total_cost FROM `timesheet` WHERE project_id=$ongoingProject->project_id AND date_added LIKE '%$date_added%'";
-                        $checkCost      = $this->db->query($cost_sql2)->getRow();
-                        $project_cost   = $checkCost->total_cost;
-                    }
+                    // if($checkCost){
+                    //     $project_cost   = $checkCost->project_cost;
+                    // } else {
+                    //     $date_added     = $last_month_year.'-'.$last_month_month;
+                    //     $cost_sql2      = "SELECT sum(cost) as total_cost FROM `timesheet` WHERE project_id=$ongoingProject->project_id AND date_added LIKE '%$date_added%'";
+                    //     $checkCost      = $this->db->query($cost_sql2)->getRow();
+                    //     $project_cost   = $checkCost->total_cost;
+                    // }
+                    $date_added     = $last_month_year.'-'.$last_month_month;
+                    $cost_sql2      = "SELECT sum(cost) as total_cost FROM `timesheet` WHERE project_id=$ongoingProject->project_id AND date_added >= '$startDate' AND date_added <= '$endDate'";
+                    $checkCost      = $this->db->query($cost_sql2)->getRow();
+                    $project_cost   = $checkCost->total_cost;
                     $total_cost += $project_cost;
                 /* cost calculation */
                 $html .= '<tr>
