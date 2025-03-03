4cbefacbb3c3b0b7d0ecaddaa101037782fcd54b
Deblina Das <deblina@keylines.net>
2025-03-03 11:28:56 +0530

    report modification

--- app/Views/admin/maincontents/report/hours-report.php
+++ app/Views/admin/maincontents/report/hours-report.php
@@ -201,7 +201,7 @@
         </div>
     <?php } ?>
 </section>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
     function dayWiseListGenerate(value) {
         console.log("Selected value: " + value);
@@ -262,7 +262,7 @@
         setTimeout(function(){newWin.close();},10);
     }
 </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 <!-- <script>
 $(document).ready(function () {
