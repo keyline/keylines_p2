b5553c8f86ca0e6d33d1c6f94d156deaa6ced71d
Deblina Das <deblina@keylines.net>
2025-05-17 18:18:39 +0530

    punch in time show

--- app/Views/admin/maincontents/task-assign/list.php
+++ app/Views/admin/maincontents/task-assign/list.php
@@ -686,7 +686,7 @@ $controller_route       = $moduleDetail['controller_route'];
 
                                                                     $checkAttnendance = $common_model->find_data('attendances', 'count', ['user_id' => $teamMember->id, 'punch_date' => date('Y-m-d')]);                                                                    
                                                                     $checkAttnendancetime = $common_model->find_data('attendances', 'row', ['user_id' => $teamMember->id, 'punch_date' => date('Y-m-d')]);
-                                                                    $sql = "SELECT * FROM attendances WHERE user_id = $teamMember->id AND punch_date = date('Y-m-d') LIMIT 1";
+                                                                    $sql = "SELECT * FROM attendances WHERE user_id = $teamMember->id AND punch_date = 'date('Y-m-d')' LIMIT 1";
                                                                     echo $sql;
                                                                     
                                                                     if($checkAttnendance > 0){
