97e874dda9ea2a8accea6a2539d16beff97c6c32
Deblina Das <deblina@keylines.net>
2025-05-17 18:05:42 +0530

    punch in time show

--- app/Views/admin/maincontents/task-assign/list.php
+++ app/Views/admin/maincontents/task-assign/list.php
@@ -392,7 +392,8 @@ $controller_route       = $moduleDetail['controller_route'];
                                                                     $totalAssigned    = '[Assigned : ' . $totalAssigned . ']';
 
                                                                     $checkAttnendance = $common_model->find_data('attendances', 'count', ['user_id' => $teamMember->id, 'punch_date' => $yesterday]);                                                                
-                                                                    $checkAttnendancetime = $common_model->find_data('attendances', 'row', ['user_id' => $teamMember->id, 'punch_date' => $yesterday]); die;
+                                                                    $checkAttnendancetime = $common_model->find_data('attendances', 'row', ['user_id' => $teamMember->id, 'punch_date' => $yesterday]);
+                                                                    dd($checkAttnendancetime);
                                                                     // echo $this->db->last_query();
                                                                     // pr($checkAttnendancetime);
                                                                     if($checkAttnendancetime){
