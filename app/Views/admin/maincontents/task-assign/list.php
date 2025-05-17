b2457482cf118b66e61fa9cce301a4dd1aed0050
Deblina Das <deblina@keylines.net>
2025-05-17 18:24:00 +0530

    punch in time show

--- app/Views/admin/maincontents/task-assign/list.php
+++ app/Views/admin/maincontents/task-assign/list.php
@@ -685,15 +685,18 @@ $controller_route       = $moduleDetail['controller_route'];
                                                                     $totalAssigned    = '[Assigned : ' . $totalAssigned . ']';
 
                                                                     $checkAttnendance = $common_model->find_data('attendances', 'count', ['user_id' => $teamMember->id, 'punch_date' => date('Y-m-d')]);                                                                    
-                                                                    $checkAttnendancetime = $common_model->find_data('attendances', 'row', ['user_id' => $teamMember->id, 'punch_date' => date('Y-m-d')]);
-                                                                    $current =date('Y-m-d');
-                                                                    $sql = "SELECT * FROM attendances WHERE user_id = $teamMember->id AND punch_date = $current LIMIT 1";
-                                                                    echo $sql;
+                                                                    
+                                                                    // $current =date('Y-m-d');
+                                                                    // $sql = "SELECT * FROM attendances WHERE user_id = $teamMember->id AND punch_date = $current LIMIT 1";
+                                                                    // echo $sql;
                                                                     
                                                                     if($checkAttnendance > 0){
+                                                                        $checkAttnendancetime = $common_model->find_data('attendances', 'row', ['user_id' => $teamMember->id, 'punch_date' => date('Y-m-d')]);
+                                                                        $punchInTime = date_format(date_create($checkAttnendancetime->punch_in), "h:i A");
                                                                         $attnBgColor = '#d1fa05';
                                                                     } else {
                                                                         $attnBgColor = 'red';
+                                                                        $punchInTime = '';
                                                                     }
                                                                     $checkBooking = $common_model->find_data('timesheet', 'count', ['user_id' => $teamMember->id, 'date_added' => date('Y-m-d')]);
                                                                     if($checkBooking > 0){
@@ -721,7 +724,7 @@ $controller_route       = $moduleDetail['controller_route'];
                                                                                     <span><?=$teamMember->name?></span>
                                                                                 </div>
                                                                                 <div class="col-md-6" style="text-align: left;">
-                                                                                    <span style="padding: 2px 8px; border-radius: 10px; font-size:10px; background-color:<?=$attnBgColor?>; color: #000;">Punch-In</span><br>
+                                                                                    <span style="padding: 2px 8px; border-radius: 10px; font-size:10px; background-color:<?=$attnBgColor?>; color: #000;">Punch-In: <?=$punchInTime?></span><br>
                                                                                     <span style="padding: 2px 8px; border-radius: 10px; font-size:10px; background-color:<?=$trackerBgColor?>; color: #000;">Tracker</span>
                                                                                 </div>
                                                                                 <div class="col-md-6" style="text-align: right;">
