b4c5e0df1ab50d472dc0471b8eac994fd5d13597
Deblina Das <deblina@keylines.net>
2025-05-17 17:44:11 +0530

    punch in time show

--- app/Views/admin/maincontents/task-assign/list.php
+++ app/Views/admin/maincontents/task-assign/list.php
@@ -393,6 +393,9 @@ $controller_route       = $moduleDetail['controller_route'];
 
                                                                     $checkAttnendance = $common_model->find_data('attendances', 'count', ['user_id' => $teamMember->id, 'punch_date' => $yesterday]);
                                                                     $checkAttnendancetime = $common_model->find_data('attendances', 'row', ['user_id' => $teamMember->id, 'punch_date' => $yesterday]);
+                                                                    if($checkAttnendancetime){
+                                                                        $punchInTime = date_format(date_create($checkAttnendancetime->punch_in), "h:i A");                                                                       
+                                                                    } 
                                                                     if($checkAttnendance > 0){
                                                                         $attnBgColor = '#d1fa05';
                                                                     } else {
@@ -423,7 +426,7 @@ $controller_route       = $moduleDetail['controller_route'];
                                                                                     <span><?=$teamMember->name?></span>
                                                                                 </div>
                                                                                 <div class="col-md-6" style="text-align: left;">
-                                                                                    <span style="padding: 2px 8px; border-radius: 10px; font-size:10px; background-color:<?=$attnBgColor?>; color: #000;">Punch-In: <?=$checkAttnendancetime?></span><br>
+                                                                                    <span style="padding: 2px 8px; border-radius: 10px; font-size:10px; background-color:<?=$attnBgColor?>; color: #000;">Punch-In: <?=$punchInTime?></span><br>
                                                                                     <span style="padding: 2px 8px; border-radius: 10px; font-size:10px; background-color:<?=$trackerBgColor?>; color: #000;">Tracker</span>
                                                                                 </div>
                                                                                 <div class="col-md-6" style="text-align: right;">
