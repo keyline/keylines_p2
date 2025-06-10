15055f3fe0fa88344d6144cf8df9928d181c3754
Deblina Das <deblina@keylines.net>
2025-06-10 17:10:46 +0530

    notification

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -283,7 +283,7 @@ abstract class BaseController extends Controller
 
     public function sendFCMMessage($accessToken, $projectId, $message)
     {
-        pr($message);
+        // pr($message);
         $url = "https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send";
 
         $headers = [
