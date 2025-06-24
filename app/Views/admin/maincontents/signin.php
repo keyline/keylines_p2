7ce8778897192ca99a57357a60f85cb5df0584fa
Deblina Das <deblina@keylines.net>
2025-06-24 18:56:23 +0530

    modification

--- app/Views/admin/maincontents/signin.php
+++ app/Views/admin/maincontents/signin.php
@@ -68,13 +68,13 @@
           <div class="col-md-5">
             <img src="<?= base_url('public/uploads/Android.png')?>" alt="" class="img-fluid">
             <a href="https://play.google.com/store/apps/details?id=com.keytracker.keyline">
-                <img src="<?= base_url('public/uploads/play-store.png')?>" alt="" class="img-fluid">
+                <img src="<?= base_url('public/uploads/play-store.png')?>" alt="" class="img-fluid" target ="_blank">
             </a>
           </div>
           <div class="col-md-5">
             <img src="<?= base_url('public/uploads/iOS.png')?>" alt="" class="img-fluid">
             <a href="https://apps.apple.com/us/app/effortrak/id6502506223">
-                <img src="<?= base_url('public/uploads/app-store.png')?>" alt="" class="img-fluid">
+                <img src="<?= base_url('public/uploads/app-store.png')?>" alt="" class="img-fluid" target ="_blank">
             </a>
           </div>
         </div>
