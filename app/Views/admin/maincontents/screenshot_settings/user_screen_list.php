18649a16a059e7df7137db5ad1e71fc986d04950
surajit bera <surajit.b@keylines.net>
2025-08-07 18:43:48 +0530

    user name add

--- app/Views/admin/maincontents/screenshot_settings/user_screen_list.php
+++ app/Views/admin/maincontents/screenshot_settings/user_screen_list.php
@@ -55,7 +55,7 @@
                     <!-- ____ code ____ -->
 
                     <div class="container my-4">
-                        <h6><?= $user->name ?></h6>
+                        <h6 class="mb-3"><?= $user->name ?></h6>
                         <div class="row g-3">
                             <?php if (count($row)) {
                                 foreach ($row as $screenshot) { ?>
