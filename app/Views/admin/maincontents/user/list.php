4d01709b740ba283782d0c264e4da9d402599c69
surajit bera <surajit.b@keylines.net>
2025-08-08 16:44:34 +0530

    ui work

--- app/Views/admin/maincontents/user/list.php
+++ app/Views/admin/maincontents/user/list.php
@@ -81,10 +81,12 @@ $controller_route   = $moduleDetail['controller_route'];
                                                 <td class="text-center"><?= $row->phone1 ?></td>
                                                 <td><?= $row->email ?></td>
                                                 <td>
-                                                    <a href="<?= base_url('admin/user/screenshots/' . encoded($row->id)); ?>" class="badge bg-custom-primary">
+                                                    <a href="<?= base_url('admin/user/screenshots/' . encoded($row->id)); ?>">
                                                         <?php if (!empty($row->screenshot_time)): ?>
-                                                            <img src="<?= getenv('app.uploadsURL') ?>/time-check.png" alt="" style="width: 4%;"> <?= date('g:i A', strtotime($row->screenshot_time)); ?>
-                                                            <!-- <i class="fas fa-image"></i> -->
+                                                            <span class="badge bg-custom-primary">
+                                                                <img src="<?= getenv('app.uploadsURL') ?>/time-check.png" alt="" style="width: 4%;"> <?= date('g:i A', strtotime($row->screenshot_time)); ?>
+                                                                <!-- <i class="fas fa-image"></i> -->
+                                                            </span>
                                                         <?php endif; ?>
                                                     </a>
                                                 </td>
