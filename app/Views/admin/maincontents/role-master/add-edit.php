84dd3e70e246ca7afa825031cff23cd4d455bff6
surajit bera <surajit.b@keylines.net>
2025-08-11 18:48:01 +0530

    ui work

--- app/Views/admin/maincontents/role-master/add-edit.php
+++ app/Views/admin/maincontents/role-master/add-edit.php
@@ -7,38 +7,40 @@ if ($row) {
 ?>
 <script src="//cdn.ckeditor.com/4.13.1/full/ckeditor.js"></script>
 <div class="pcoded-content">
-    <div class="page-header">
-        <div class="page-block">
-            <div class="row align-items-center">
-                <div class="col-md-12">
-                    <div class="page-header-title pagetitle">
-                        <h1 class="m-b-10"><?php echo $page_header; ?></h1>
+    <div class="container-fluid">
+        <div class="page-header">
+            <div class="page-block">
+                <div class="row align-items-center">
+                    <div class="col-md-12">
+                        <div class="page-header-title pagetitle">
+                            <h1 class="m-b-10"><?php echo $page_header; ?></h1>
+                        </div>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="<?php echo base_url('admin/dashboard'); ?>">Home </a></li>
+                            <li class="breadcrumb-item"><a href="#!"><?php echo $page_header; ?></a></li>
+                        </ul>
                     </div>
-                    <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="<?php echo base_url('admin/dashboard'); ?>">Home </a></li>
-                        <li class="breadcrumb-item"><a href="#!"><?php echo $page_header; ?></a></li>
-                    </ul>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-12">
-            <form action="" method="post" enctype="multipart/form-data">
-                <div class="box-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="module_name">Role Master Name <span style="color:red;">*</span></label>
-                                <input type="text" class="form-control" name="role_master_name" id="role_master_name" value="<?= $name ?>" required>
+        <div class="row">
+            <div class="col-sm-12">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="module_name">Role Master Name <span style="color:red;">*</span></label>
+                                    <input type="text" class="form-control" name="role_master_name" id="role_master_name" value="<?= $name ?>" required>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="text-center" style="margin-top: 10px;">
-                    <input type="submit" class="btn btn-primary" name="submit" value="Submit">
-                </div>
-            </form>
+                    <div class="text-center" style="margin-top: 10px;">
+                        <input type="submit" class="btn btn-primary" name="submit" value="Submit">
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
