9257d484b00d60c9dfc56193575dff38c83181df
surajit bera <surajit.b@keylines.net>
2025-08-12 18:25:45 +0530

    ui work

--- app/Views/admin/maincontents/project-status/add-edit.php
+++ app/Views/admin/maincontents/project-status/add-edit.php
@@ -3,62 +3,70 @@ $title              = $moduleDetail['title'];
 $primary_key        = $moduleDetail['primary_key'];
 $controller_route   = $moduleDetail['controller_route'];
 ?>
-<div class="pagetitle">
-    <h1><?=$page_header?></h1>
-    <nav>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="<?=base_url('admin/dashboard')?>">Home</a></li>
-            <li class="breadcrumb-item active"><a href="<?=base_url('admin/' . $controller_route . '/list/')?>"><?=$title?> List</a></li>
-            <li class="breadcrumb-item active"><?=$page_header?></li>
-        </ol>
-    </nav>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="pagetitle">
+                <h1><?=$page_header?></h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?=base_url('admin/dashboard')?>">Home</a></li>
+                        <li class="breadcrumb-item active"><a href="<?=base_url('admin/' . $controller_route . '/list/')?>"><?=$title?> List</a></li>
+                        <li class="breadcrumb-item active"><?=$page_header?></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- End Page Title -->
 <section class="section profile">
-    <div class="row">
-        <div class="col-xl-12">
-            <?php if(session('success_message')){?>
-            <div class="alert alert-success bg-success text-light border-0 alert-dismissible fade show hide-message custom-alert" role="alert">
-                <?=session('success_message')?>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-12">
+                <?php if(session('success_message')){?>
+                <div class="alert alert-success bg-success text-light border-0 alert-dismissible fade show hide-message custom-alert" role="alert">
+                    <?=session('success_message')?>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php }?>
+                <?php if(session('error_message')){?>
+                <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show hide-message custom-alert" role="alert">
+                    <?=session('error_message')?>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php }?>
             </div>
-            <?php }?>
-            <?php if(session('error_message')){?>
-            <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show hide-message custom-alert" role="alert">
-                <?=session('error_message')?>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-            <?php }?>
-        </div>
-        <?php
-            if($row){
-              $name     = $row->name;
-            } else {
-              $name     = '';
-            }
-            ?>
-        <div class="col-xl-12">
-            <div class="card">
-                <div class="card-body pt-3">
-                    <form method="POST" action="" enctype="multipart/form-data" class="general_form_style">
-                        <div class="container-fluid">
-                            <div class="row mb-2">
-                                <div class="col-md-2 col-lg-2">
-                                    <div class="general_form_left_box">
-                                        <label for="name" class="col-form-label"><?=$title?> Name</label>
+            <?php
+                if($row){
+                $name     = $row->name;
+                } else {
+                $name     = '';
+                }
+                ?>
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-body pt-3">
+                        <form method="POST" action="" enctype="multipart/form-data" class="general_form_style">
+                            <div class="container-fluid">
+                                <div class="row mb-2">
+                                    <div class="col-md-2 col-lg-2">
+                                        <div class="general_form_left_box">
+                                            <label for="name" class="col-form-label"><?=$title?> Name</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-10 col-lg-10">
+                                        <div class="">
+                                            <input type="text" name="name" class="form-control" id="name" value="<?=$name?>" required>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-md-10 col-lg-10">
-                                    <div class="general_form_right_box">
-                                        <input type="text" name="name" class="form-control" id="name" value="<?=$name?>" required>
-                                    </div>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary btn-sm small-btn"><?=(($row)?'Save':'Add')?></button>
                                 </div>
                             </div>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary btn-sm small-btn"><?=(($row)?'Save':'Add')?></button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
