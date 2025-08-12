c4a7725b2ddfc05ca7d12f396d7bb206ff4f25b5
surajit bera <surajit.b@keylines.net>
2025-08-12 18:46:07 +0530

    ui work

--- app/Views/admin/maincontents/holiday-list.php
+++ app/Views/admin/maincontents/holiday-list.php
@@ -25,130 +25,138 @@ $controller_route   = $moduleDetail['controller_route'];
         padding: 10px;
     }
 </style>
-<div class="pagetitle">
-    <h1><?= $page_header ?></h1>
-    <nav>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="<?= base_url('admin/dashboard') ?>">Home</a></li>
-            <li class="breadcrumb-item active"><a href="<?= base_url('admin/' . $controller_route . '/list/') ?>"><?= $title ?> List</a></li>
-            <li class="breadcrumb-item active"><?= $page_header ?></li>
-        </ol>
-    </nav>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="pagetitle">
+                <h1><?= $page_header ?></h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?= base_url('admin/dashboard') ?>">Home</a></li>
+                        <li class="breadcrumb-item active"><a href="<?= base_url('admin/' . $controller_route . '/list/') ?>"><?= $title ?> List</a></li>
+                        <li class="breadcrumb-item active"><?= $page_header ?></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- End Page Title -->
 <section class="section profile">
-    <?php if (checkModuleFunctionAccess(26, 49)) { ?>
-        <div class="row">
-            <div class="col-xl-12">
-                <?php if (session('success_message')) { ?>
-                    <div class="alert alert-success bg-success text-light border-0 alert-dismissible fade show hide-message" role="alert">
-                        <?= session('success_message') ?>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                <?php } ?>
-                <?php if (session('error_message')) { ?>
-                    <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show hide-message" role="alert">
-                        <?= session('error_message') ?>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                <?php } ?>
-            </div>
-            <div class="col-xl-6">
-                <div class="card">
-                    <div class="card-body pt-3">
-                        <div class="portlet box green">
-                            <div class="portlet-title">
-                                <div class="caption"></div>
-                                <!-- <div class="tools"> </div> -->
+    <div class="container-fluid">
+        <?php if (checkModuleFunctionAccess(26, 49)) { ?>
+            <div class="row">
+                <div class="col-xl-12">
+                    <?php if (session('success_message')) { ?>
+                        <div class="alert alert-success bg-success text-light border-0 alert-dismissible fade show hide-message" role="alert">
+                            <?= session('success_message') ?>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    <?php } ?>
+                    <?php if (session('error_message')) { ?>
+                        <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show hide-message" role="alert">
+                            <?= session('error_message') ?>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="col-xl-6">
+                    <div class="card">
+                        <div class="card-body pt-3">
+                            <div class="portlet box green">
+                                <div class="portlet-title">
+                                    <div class="caption"></div>
+                                    <!-- <div class="tools"> </div> -->
+                                </div>
+                                <div class="portlet-body">
+                                    <div id="calendar">
+                                        <center>
+                                            <span class="dot" style="background:red" ></span>&nbsp;All 1st,3rd Saturday and Sunday &nbsp;&nbsp;
+                                        </center>
+                                    </div>                        
+                                </div>
                             </div>
-                            <div class="portlet-body">
-                                <div id="calendar">
-                                    <center>
-                                        <span class="dot" style="background:red" ></span>&nbsp;All 1st,3rd Saturday and Sunday &nbsp;&nbsp;
-                                    </center>
-                                </div>                        
+                            <!-- Modal -->
+                            <div class="modal" id="holidayModal">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">        
+                                        <div class="modal-header py-1">
+                                        <h5 class="modal-title">Create event for <span id="eventDate1"></span></h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                        <div class="modal-body">
+                                            <form id="holidayForm" method="POST">
+                                                <input type="hidden" name="start_event" id="start_event" />
+                                                <input type="hidden" name="end_event" id="end_event" />
+                                                <input type="hidden" id="holidayId">
+                                                <div class="form-group">              
+                                                    <input type="text" class="form-control requiredCheck" data-check="Event Title" name="title" id="title" aria-describedby="helpId" placeholder="Enter Event title">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="color_code_bc">Background Color</label>
+                                                    <input type="color" class="form-control requiredCheck" data-check="Color Code" name="color_code_bc" id="color_code_bc" aria-describedby="helpId" placeholder="Select Background Color Code" value="#FFFFFF">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="color_code_fc">Font Color</label>
+                                                    <input type="color" class="form-control requiredCheck" data-check="Color Code" name="color_code_fc" id="color_code_fc" aria-describedby="helpId" placeholder="Select Font Color Code" value="#000000">
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" id="dataForm" class="btn btn-primary btn-sm mt-2" data-dismiss="modal">Save</button>                          
+                                                </div> 
+                                            </form>                   
+                                        </div>        
+                                    </div>                                                
+                                </div>
                             </div>
                         </div>
-                        <!-- Modal -->
-                        <div class="modal" id="holidayModal">
-                            <div class="modal-dialog">
-                                <div class="modal-content">        
-                                    <div class="modal-header">
-                                    <h4 class="modal-title">Create event for <span id="eventDate1"></span></h4>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>                
+                </div>
+                <div class="col-md-6"> 
+                    <div class="card">                                           
+                        <div class="row">
+                            <div class="portlet box green portlet-right card table-card">
+                                <div class="portlet-title">
+                                    <div class="caption">
+                                    </div>
+                                    <div class="tools">
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-success btn-sm m-3" data-bs-toggle="modal" data-bs-target="#holidayAddModal">
+                                        Add New Event
+                                        </button>
+                                    </div>
                                 </div>
-                                    <div class="modal-body">
-                                        <form id="holidayForm" method="POST">
-                                            <input type="hidden" name="start_event" id="start_event" />
-                                            <input type="hidden" name="end_event" id="end_event" />
-                                            <input type="hidden" id="holidayId">
-                                            <div class="form-group">              
-                                                <input type="text" class="form-control requiredCheck" data-check="Event Title" name="title" id="title" aria-describedby="helpId" placeholder="Enter Event title">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="color_code_bc">Background Color</label>
-                                                <input type="color" class="form-control requiredCheck" data-check="Color Code" name="color_code_bc" id="color_code_bc" aria-describedby="helpId" placeholder="Select Background Color Code" value="#FFFFFF">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="color_code_fc">Font Color</label>
-                                                <input type="color" class="form-control requiredCheck" data-check="Color Code" name="color_code_fc" id="color_code_fc" aria-describedby="helpId" placeholder="Select Font Color Code" value="#000000">
-                                            </div>
-                                            <div class="form-group">
-                                                <button type="submit" id="dataForm" class="btn btn-primary" data-dismiss="modal">Save</button>                          
-                                            </div> 
-                                        </form>                   
-                                    </div>        
-                                </div>                                                
-                            </div>
-                        </div>
-                    </div>
-                </div>                
-            </div>
-            <div class="col-md-6"> 
-                <div class="card">                                           
-                    <div class="row">
-                        <div class="portlet box green portlet-right card table-card">
-                            <div class="portlet-title">
-                                <div class="caption">
+                                <div class="portlet-body">
+                                    <table class="table general_table_style padding-y-10" id="event-table">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Event Name</th>
+                                                <th>Event Date</th>
+                                                <th>Action</th>                                            
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php                                                    
+                                            if($events){ $sl=1; foreach($events as $row){ ?>
+                                            <tr>
+                                            <th scope="row"><?=$sl++?></th>
+                                            <th scope="row"><?=$row->title?></th>
+                                            <th scope="row"><?=date_format(date_create($row->start_event), "l, M d, Y")?></th>
+                                            <th scope="row">                                            
+                                                <a href="<?=base_url('admin/' . $controller_route . '/delete/'.encoded($row->$primary_key))?>" class="btn btn-outline-danger btn-sm" title="Delete <?=$title?>" onclick="return confirm('Do You Want To Delete This <?=$title?>');"><i class="fa fa-trash"></i></a>
+                                            </th>
+                                            </tr>
+                                        <?php } }?>
+                                        </tbody>
+                                    </table>
                                 </div>
-                                <div class="tools">
-                                    <!-- Button trigger modal -->
-                                    <button type="button" class="btn btn-success btn-sm m-3" data-bs-toggle="modal" data-bs-target="#holidayAddModal">
-                                      Add New Event
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="portlet-body">
-                                <table class="table general_table_style padding-y-10" id="event-table">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Event Name</th>
-                                            <th>Event Date</th>
-                                            <th>Action</th>                                            
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    <?php                                                    
-                                        if($events){ $sl=1; foreach($events as $row){ ?>
-                                        <tr>
-                                        <th scope="row"><?=$sl++?></th>
-                                        <th scope="row"><?=$row->title?></th>
-                                        <th scope="row"><?=date_format(date_create($row->start_event), "l, M d, Y")?></th>
-                                        <th scope="row">                                            
-                                            <a href="<?=base_url('admin/' . $controller_route . '/delete/'.encoded($row->$primary_key))?>" class="btn btn-outline-danger btn-sm" title="Delete <?=$title?>" onclick="return confirm('Do You Want To Delete This <?=$title?>');"><i class="fa fa-trash"></i></a>
-                                        </th>
-                                        </tr>
-                                    <?php } }?>
-                                    </tbody>
-                                </table>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php } ?>
+        <?php } ?>
+    </div>
 </section>
 
 <!-- Modal -->
