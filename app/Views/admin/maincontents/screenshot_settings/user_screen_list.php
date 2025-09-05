efb9b0948e99bf2dfb29182c37f0d2345395bb14
surajit bera <surajit.b@keylines.net>
2025-09-05 15:19:14 +0530

    ui work on 5-09-2025

--- app/Views/admin/maincontents/screenshot_settings/user_screen_list.php
+++ app/Views/admin/maincontents/screenshot_settings/user_screen_list.php
@@ -1016,7 +1016,7 @@
                                        </?php } ?>
                                     </div>
                                 </div> -->
-                                <div class="col-7">
+                                <div class="col-6">
                                     <div class="row">
                                         <div class="col-md-4">
                                             <div class="totalWorking time_box">
